0101f15859ab5372e0c02aea734e115046b36751
Piotr Czerski <piotr.czerski10@gmail.com>
2020-01-11 03:49:28 +0100

    First commit new file

---
+++ ImpactIT/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImpactIT.Delegaty;
+
+
+namespace ImpactIT
+{
+    class Program
+    {
+        public delegate void SomeMetgodDel(); //tutaj tworzymy delegat
+        static void Main(string[] args)
+        {
+            
+
+             SomeMetgodDel someDele = SomeMethod; //tutaj robimy wskaźnik
+             someDele.Invoke();
+
+             Console.ReadKey();
+
+            SomeLongRunningData sm = new SomeLongRunningData();
+            sm.SomeMethod(CallBackMethod);
+
+            Console.ReadKey();
+        }
+
+
+        public static void SomeMethod()
+        {
+            Console.WriteLine("Jestem tu !!");
+        }
+
+        static void CallBackMethod(int i)
+        {
+          Console.WriteLine(i);
+        }
+    }
+    class SomeLongRunningData
+    {
+        public delegate void CallBack(int i);
+        public void SomeMethod(CallBack obj)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                //Robi się coś w tle
+                obj(i);
+            }
+        }
+    }
+}
