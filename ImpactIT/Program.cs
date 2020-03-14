9c2159b889c7dd886aa516dfb77dcc0a0e886ed8
Piotr Czerski <piotr.czerski10@gmail.com>
2020-03-14 12:21:19 +0100

    Update file

--- ImpactIT/Program.cs
+++ ImpactIT/Program.cs
@@ -3,49 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ImpactIT.Delegaty;
 
 
 namespace ImpactIT
 {
     class Program
     {
-        public delegate void SomeMetgodDel(); //tutaj tworzymy delegat
+        //public delegate void SomeMetgodDel(); //tutaj tworzymy delegat
+
         static void Main(string[] args)
         {
+
+
+            //SomeMetgodDel someDele = SomeMethod; //tutaj robimy wskaźnik
+            //someDele.Invoke();
             
-
-             SomeMetgodDel someDele = SomeMethod; //tutaj robimy wskaźnik
-             someDele.Invoke();
-
-             Console.ReadKey();
-
             SomeLongRunningData sm = new SomeLongRunningData();
             sm.SomeMethod(CallBackMethod);
+            
+            //Dynamic
+
+         //   string tojeststring = "aa";
+         //
+         //   var mvar = 1 + 3;
+         //   dynamic dyn = 1 + 3;
+         //   object obj = 1 + 3;
+         //   Console.WriteLine(mvar.GetType());
+         //   Console.WriteLine(dyn.GetType());
+         //   Console.WriteLine(obj.GetType());
+         /*
+            dynamic dyna;
+            int a = 20;
+
+            dyna = a;
+
+            string b = "dynamic";
+
+            dyna = b;
+
+            DateTime dt = DateTime.Now;
+
+            dyna = dt;
+            */
 
             Console.ReadKey();
         }
-
-
+        /*
         public static void SomeMethod()
         {
             Console.WriteLine("Jestem tu !!");
         }
-
+        */
         static void CallBackMethod(int i)
         {
-          Console.WriteLine(i);
+            Console.WriteLine(i);
         }
-    }
-    class SomeLongRunningData
-    {
-        public delegate void CallBack(int i);
-        public void SomeMethod(CallBack obj)
+        
+
+        public class SomeLongRunningData
         {
-            for (int i = 0; i < 1000; i++)
+            public delegate void CallBack(int i);
+
+            public void SomeMethod(CallBack obj)
             {
-                //Robi się coś w tle
-                obj(i);
+                for (int i = 0; i < 1000; i++)
+                {
+                    //Robi się coś w tle
+                    obj(i);
+                }
             }
         }
     }
